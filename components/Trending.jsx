d42afeb9b705bd67481ac81bd7e3e744a01cce26
tompujalte <tom.pujalte@gmail.com>
2024-10-22 17:25:03 +0200

    home page fonctionnality

---
+++ components/Trending.jsx
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { FlatList } from 'react-native'
+
+const Trending = ({ posts }) => {
+  return (
+    <FlatList 
+    data={posts}
+    keyExtractor={(item) => item.$id}
+    renderItem={({ item }) => <Text className="text-white text-3xl">{item.id}</Text>}
+    horizontal
+    />
+  )
+}
+
+export default Trending
